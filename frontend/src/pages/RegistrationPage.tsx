5655bacafb8ab97de62314d7d612c1acf2e51869
Edwin Gabriel <65310253+Edwinvilla72@users.noreply.github.com>
2025-04-09 13:45:41 +0000

    testing

--- frontend/src/pages/RegistrationPage.tsx
+++ frontend/src/pages/RegistrationPage.tsx
@@ -1,10 +1,10 @@
-import Register from '../components/Register.tsx';
+//import Register from '../components/Register.tsx';
 const RegistrationPage = () => {
     return (
         <div>
             <h1 id="registerTitle">Sign up for FitGame!</h1>
-            <Register />
+            
         </div>
     );
 };
-export default RegistrationPagePage;
+export default RegistrationPage;
