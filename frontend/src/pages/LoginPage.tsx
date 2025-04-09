58390320a06c8ad7262d806ce8b0358cf5d0caa6
Edwin Gabriel <65310253+Edwinvilla72@users.noreply.github.com>
2025-04-09 13:44:46 +0000

    .

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,5 +1,6 @@
 import PageTitle from '../components/PageTitle.tsx';
 import Login from '../components/Login.tsx';
+import Register from '../components/Register.tsx';
 const LoginPage = () => {
     return (
         <div>
