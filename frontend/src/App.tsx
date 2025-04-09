077cbde13b9f2867d91f818c70277aa68bafa7c1
Edwin Gabriel <65310253+Edwinvilla72@users.noreply.github.com>
2025-04-09 13:50:25 +0000

    Trying to remove literally every reference to registration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
-import RegisterPage from './pages/RegisterPage';
 function App() {
   return (
     <Router >
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/cards" element={<CardPage />} />
-        <Route path="/register" element={<RegistrationPage />}/>
       </Routes>
     </Router>
   );
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,6 +1,5 @@
 import PageTitle from '../components/PageTitle.tsx';
 import Login from '../components/Login.tsx';
-//import Register from '../components/Register.tsx';
 const LoginPage = () => {
     return (
         <div>
