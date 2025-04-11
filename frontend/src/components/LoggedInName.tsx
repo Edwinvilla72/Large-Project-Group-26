2513bc198a9764f34a3c694d2f2e453a754079e3
Edwinvilla72 <Edwin.villa2@icloud.com>
2025-04-11 04:21:22 -0400

    Even smaller update

--- frontend/src/components/LoggedInName.tsx
+++ frontend/src/components/LoggedInName.tsx
@@ -20,18 +20,18 @@ function LoggedInName() {
     return (
         <>
           <motion.div
-            initial={{ x: -100, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            exit={{ x: 100, opacity: 0 }}
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 100, opacity: 0 }}
             transition={{ duration: 0.4 }}
           >
             <span id="userName">Welcome back, {firstName} {lastName}!</span><br />
           </motion.div>
       
           <motion.div
-            initial={{ x: -100, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            exit={{ x: 100, opacity: 0 }}
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 100, opacity: 0 }}
             transition={{ duration: 0.4 }}
           >
             <button
