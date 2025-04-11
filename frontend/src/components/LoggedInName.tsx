7696343fc14dc106ce78a4dbff9a0be8fd7b3654
Edwinvilla72 <Edwin.villa2@icloud.com>
2025-04-11 04:20:28 -0400

    smaller update

--- frontend/src/components/LoggedInName.tsx
+++ frontend/src/components/LoggedInName.tsx
@@ -1,6 +1,4 @@
 import { motion } from 'framer-motion'; // to animate pages ooooooo
-import { useNavigate } from 'react-router-dom'; // if you're using React Router
-
 
 function LoggedInName() {
     // user variables
@@ -10,32 +8,31 @@ function LoggedInName() {
     var userId = ud._id;
     var firstName = ud.FirstName;
     var lastName = ud.LastName;
-    const navigate = useNavigate(); // for redirecting
 
     // logout function
     function doLogout(event: any): void {
         event.preventDefault();
         localStorage.removeItem("user_data")
-        navigate('/');
+        window.location.href = '/';
     };
 
     // additions to the page which includes the users first and last name (adds a log out button)
     return (
         <>
           <motion.div
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: 100, opacity: 0 }}
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: 100, opacity: 0 }}
             transition={{ duration: 0.4 }}
           >
             <span id="userName">Welcome back, {firstName} {lastName}!</span><br />
           </motion.div>
       
           <motion.div
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: 100, opacity: 0 }}
-            transition={{ duration: 0.42 }}
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: 100, opacity: 0 }}
+            transition={{ duration: 0.4 }}
           >
             <button
               type="button"
