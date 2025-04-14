547627b7e8b9c4027fcde37f4f35b0c4ebca009e
Michael Rivera <levelcoolio767@gmail.com>
2025-04-14 11:11:24 -0400

    Removed first/last, isn't used

--- backend/User.js
+++ backend/User.js
@@ -15,14 +15,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
     character: {
         name: String,
         level: Number,
