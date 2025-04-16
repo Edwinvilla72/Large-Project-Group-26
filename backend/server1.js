13eee3492c54994da5c73869bf227b77d7a5e541
VicRoj123 <victaaa01@gmail.com>
2025-04-16 19:49:08 -0400

    deleted commented out block
    was not needed

--- backend/server1.js
+++ backend/server1.js
@@ -25,43 +25,6 @@ function authenticateToken(req, res, next) {
 
 
 connectDB();
-/*const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb+srv://Edwin123:12345@cluster0.jqhcjet.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const client = new MongoClient(url);
-client.connect();
-
-// test
-client.connect()
-    .then(async () => {
-        console.log("✅ Connected to MongoDB!");
-
-        // Quick test: List databases
-        const databasesList = await client.db().admin().listDatabases();
-        console.log("📂 Databases:");
-        databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-    })
-    .catch(err => {
-        console.error("❌ Failed to connect to MongoDB:", err);
-    });
-
-
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS'
-    );
-    next();
-});
-app.listen(5000); // start Node + Express server on port 5000
-*/
 
 const app = express();
 app.use(express.json());
