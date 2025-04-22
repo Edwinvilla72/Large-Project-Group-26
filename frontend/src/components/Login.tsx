841e42d981b13a172b899973bac537a7eb0b0a16
Edwinvilla72 <Edwin.villa2@icloud.com>
2025-04-21 20:11:34 -0400

    changed direction after login from cards to dashboard

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -50,7 +50,7 @@ function Login() {
 
       localStorage.setItem('user_data', JSON.stringify(user));
       setMessage('');
-      navigate('/cards');
+      navigate('/Dashboard');
     } catch (error: any) {
       console.error('Login error:', error);
       setMessage('Server error. Please try again later.');
